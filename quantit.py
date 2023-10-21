e9f20860da2dc5b7673a62e77cb7d3b46e2406f7
Hussan Khan <hussankhanwork@gmail.com>
2023-10-20 20:14:23 -0500

    Add files via upload

---
+++ quantit.py
@@ -0,0 +1,42 @@
+from optimum.gptq import GPTQQuantizer
+
+
+# Dataset id from Hugging Face
+dataset_id = "wikitext2"
+
+# GPTQ quantizer
+quantizer = GPTQQuantizer(bits=4, dataset=dataset_id, model_seqlen=4096)
+quantizer.quant_method = "gptq"
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# Hugging Face model id
+model_id = "expansezero/llama232klonglora"
+
+tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=False) # bug with fast tokenizer
+model = AutoModelForCausalLM.from_pretrained(model_id, low_cpu_mem_usage=True, torch_dtype=torch.float16) # we load the model in fp16 on purpose
+
+import os
+import json
+
+# quantize the model
+quantized_model = quantizer.quantize_model(model, tokenizer)
+
+# save the quantize model to disk
+save_folder = "./local"
+quantized_model.save_pretrained(save_folder, safe_serialization=True)
+
+# load fresh, fast tokenizer and save it to disk
+tokenizer = AutoTokenizer.from_pretrained(model_id).save_pretrained(save_folder)
+
+# save quantize_config.json for TGI
+with open(os.path.join(save_folder, "quantize_config.json"), "w", encoding="utf-8") as f:
+  quantizer.disable_exllama = False
+  json.dump(quantizer.to_dict(), f, indent=2)
+
+with open(os.path.join(save_folder, "config.json"), "r", encoding="utf-8") as f:
+  config = json.load(f)
+  config["quantization_config"]["disable_exllama"] = False
+  with open(os.path.join(save_folder, "config.json"), "w", encoding="utf-8") as f:
+    json.dump(config, f, indent=2)
