15f66f2a50ea8f93062c82727aa55025fa38ee60
Hussan Khan <hussankhanwork@gmail.com>
2023-10-23 08:27:21 -0500

    Update quantit.py

--- quantit.py
+++ quantit.py
@@ -1,59 +1,167 @@
+import time
+import os
+import logging
+
 from transformers import AutoTokenizer, TextGenerationPipeline
 from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
-import logging
-from datasets import load_dataset
-import pandas as pd
-import os
+import numpy as np
 import torch
+import torch.nn as nn
+import argparse
 
-def format_prompt(instruction, output):
-    return f"""Below is an instruction that describes a task. 
-Write a response that appropriately completes the request.
+def get_wikitext2(nsamples, seed, seqlen, tokenizer):
+    from datasets import load_dataset
+    logger = logging.getLogger(__name__)
 
-### Instruction:
-{instruction}
+    wikidata = load_dataset('wikitext', 'wikitext-2-raw-v1', split='test')
+    wikilist = [' \n' if s == '' else s for s in wikidata['text'] ]
 
-### Response:
-{output}
-"""
+    text = ''.join(wikilist)
+    logger.info("Tokenising wikitext2")
+    trainenc = tokenizer(text, return_tensors='pt')
 
-data = load_dataset('Yukang/LongAlpaca-12k', split='train')
-df = pd.concat([pd.DataFrame(data['instruction'], columns=['instruction']), pd.DataFrame(data['output'], columns=['output'])], axis=1)
+    import random
+    random.seed(seed)
+    np.random.seed(0)
+    torch.random.manual_seed(0)
 
-df['instruction_len'] = df['instruction'].apply(lambda x: len(x))
-df['output_len'] = df['output'].apply(lambda x: len(x))
+    traindataset = []
+    for _ in range(nsamples):
+        i = random.randint(0, trainenc.input_ids.shape[1] - seqlen - 1)
+        j = i + seqlen
+        inp = trainenc.input_ids[:, i:j]
+        attention_mask = torch.ones_like(inp)
+        traindataset.append({'input_ids':inp,'attention_mask': attention_mask})
+    return traindataset
 
-df['prompt'] = df.apply(lambda x: format_prompt(x['instruction'], x['output']), axis=1)
+def get_c4(nsamples, seed, seqlen, tokenizer):
+    from datasets import load_dataset
+    traindata = load_dataset(
+        'allenai/c4', 'allenai--c4', data_files={'train': 'en/c4-train.00000-of-01024.json.gz'}, split='train', use_auth_token=False
+    )
 
-logging.basicConfig(
-    format="%(asctime)s %(levelname)s [%(name)s] %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S"
-)
+    import random
+    random.seed(seed)
+    trainloader = []
+    for _ in range(nsamples):
+        while True:
+            i = random.randint(0, len(traindata) - 1)
+            trainenc = tokenizer(traindata[i]['text'], return_tensors='pt')
+            if trainenc.input_ids.shape[1] >= seqlen:
+                break
+        i = random.randint(0, trainenc.input_ids.shape[1] - seqlen - 1)
+        j = i + seqlen
+        inp = trainenc.input_ids[:, i:j]
+        attention_mask = torch.ones_like(inp)
+        trainloader.append({'input_ids':inp,'attention_mask': attention_mask})
 
-pretrained_model_dir = "expansezero/llama232klonglora"
-quantized_model_dir = "local"
+    return trainloader
 
-tokenizer = AutoTokenizer.from_pretrained(pretrained_model_dir, use_fast=True)
-df.sort_values(by='output_len', inplace=True, ascending=False)
-print(df)
-print('--------------------------------')
-examples = [tokenizer(e) for e in df['prompt'].tolist()[:256]]
-print('Tokenizing finished')
+def quantize(model_dir, output_dir, traindataset, bits, group_size, desc_act, damp, batch_size = 1, use_triton=False, trust_remote_code=False, dtype='float16'):
+    quantize_config = BaseQuantizeConfig(
+        bits=bits,
+        group_size=group_size,
+        desc_act=desc_act,
+        damp_percent=damp
+    )
 
-quantize_config = BaseQuantizeConfig(
-    bits=4,  # quantize model to 4-bit
-    group_size=128,  # it is recommended to set the value to 128
-    desc_act=False,  # set to False can significantly speed up inference but the perplexity may slightly bad
-)
+    if dtype == 'float16':
+        torch_dtype  = torch.float16
+    elif dtype == 'float32':
+        torch_dtype  = torch.float32
+    elif dtype == 'bfloat16':
+        torch_dtype  = torch.bfloat16
+    else:
+        raise ValueError(f"Unsupported dtype: {dtype}")
 
-# load un-quantized model, by default, the model will always be loaded into CPU memory
-model = AutoGPTQForCausalLM.from_pretrained(pretrained_model_dir, quantize_config)
-print('Model loaded into CPU memory')
+    logger.info(f"Loading model from {model_dir} with trust_remote_code={trust_remote_code} and dtype={torch_dtype}")
+    model = AutoGPTQForCausalLM.from_pretrained(model_dir, quantize_config=quantize_config, low_cpu_mem_usage=True, torch_dtype=torch_dtype, trust_remote_code=trust_remote_code)
 
-# quantize model, the examples should be list of dict whose keys can only be "input_ids" and "attention_mask"
-model.quantize(examples)
+    logger.info(f"Starting quantization to {output_dir} with use_triton={use_triton}")
+    start_time = time.time()
+    model.quantize(traindataset, use_triton=use_triton, batch_size=batch_size, cache_examples_on_gpu=False)
 
-# save quantized model
-model.save_quantized(quantized_model_dir)
+    logger.info(f"Time to quantize model at {output_dir} with use_triton={use_triton}: {time.time() - start_time:.2f}")
 
-# save quantized model using safetensors
-model.save_quantized(quantized_model_dir, use_safetensors=True)
+    logger.info(f"Saving quantized model to {output_dir}")
+    model.save_quantized(output_dir, use_safetensors=True)
+    logger.info("Done.")
+
+if __name__ == "__main__":
+    logger = logging.getLogger()
+
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s [%(name)s] %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+    parser = argparse.ArgumentParser(description='quantise')
+    parser.add_argument('pretrained_model_dir', type=str, help='Repo name')
+    parser.add_argument('output_dir_base', type=str, help='Output base folder')
+    parser.add_argument('dataset', type=str, help='Output base folder')
+    parser.add_argument('--trust_remote_code', action="store_true", help='Trust remote code')
+    parser.add_argument('--use_triton', action="store_true", help='Use Triton for quantization')
+    parser.add_argument('--bits', type=int, nargs='+', default=[4], help='Quantize bit(s)')
+    parser.add_argument('--group_size', type=int, nargs='+', default=[32, 128, 1024, -1], help='Quantize group size(s)')
+    parser.add_argument('--damp', type=float, nargs='+', default=[0.01], help='Quantize damp_percent(s)')
+    parser.add_argument('--desc_act', type=int, nargs='+', default=[0, 1], help='Quantize desc_act(s) - 1 = True, 0 = False')
+    parser.add_argument('--dtype', type=str, choices=['float16', 'float32', 'bfloat16'], help='Quantize desc_act(s) - 1 = True, 0 = False')
+    parser.add_argument('--seqlen', type=int, default=2048, help='Model sequence length')
+    parser.add_argument('--batch_size', type=int, default=1, help='Quantize batch size for processing dataset samples')
+    parser.add_argument('--stop_file', type=str, help='Filename to look for to stop inference, specific to this instance')
+
+    args = parser.parse_args()
+
+    stop_file = args.stop_file or ""
+
+    tokenizer = AutoTokenizer.from_pretrained(args.pretrained_model_dir, use_fast=True, trust_remote_code=args.trust_remote_code)
+
+    if args.dataset == 'wikitext':
+        traindataset = get_wikitext2(128, 0, args.seqlen, tokenizer)
+    elif args.dataset == 'c4':
+        traindataset = get_c4(128, 0, args.seqlen, tokenizer)
+    else:
+        logger.error(f"Unsupported dataset: {args.dataset}")
+        raise ValueError(f"Unsupported dataset: {args.dataset}")
+
+    abort = False
+
+    iterations=[]
+    for bits in args.bits:
+        for group_size in args.group_size:
+            for desc_act in args.desc_act:
+                for damp in args.damp:
+                    desc_act = desc_act == 1 and True or False
+                    iterations.append({"bits": bits, "group_size": group_size, "desc_act": desc_act, "damp": damp})
+
+    num_iters = len(iterations)
+    logger.info(f"Starting {num_iters} quantizations.")
+    count=1
+    for iter in iterations:
+        if not os.path.isfile("/workspace/gptq-ppl-test/STOP") and not os.path.isfile(stop_file) and not abort:
+            bits = iter['bits']
+            group_size = iter['group_size']
+            desc_act = iter['desc_act']
+            damp = iter['damp']
+
+            output_dir = args.output_dir_base
+            try:
+                os.makedirs(output_dir, exist_ok=True)
+
+                # Log file has same name as directory + .quantize.log, and is placed alongside model directory, not inside it
+                # This ensures that we can delete the output_dir in case of error or abort, without losing the logfile.
+                # Therefore the existence of the output_dir is a reliable indicator of whether a model has started or not.
+                logger.info(f"[{count} / {num_iters}] Quantizing: bits = {bits} - group_size = {group_size} - desc_act = {desc_act} - damp_percent = {damp} to {output_dir}")
+                try:
+                    quantize(args.pretrained_model_dir, output_dir, traindataset, bits, group_size, desc_act, damp, args.batch_size, args.use_triton, trust_remote_code=args.trust_remote_code, dtype=args.dtype)
+                except KeyboardInterrupt:
+                    logger.error(f"Aborted. Will delete {output_dir}")
+                    os.rmdir(output_dir)
+                    abort = True
+                except:
+                    raise
+
+            finally:
+                count += 1
+        else:
+                logger.error(f"Aborting - told to stop!")
+                break
